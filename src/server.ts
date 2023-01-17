3ae2de4d53dbb5b38a59806a85f3c906453dc8e8
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-01-16 21:01:12 -0300

    Add: Cors to fastify

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
+import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+import cors from '@fastify/cors'
 
 const SERVER_PORT = 3333
 const server = Fastify()
+server.register(cors, { origin: ['http://localhost:3000'] })
 
-server.get('/', () => { })
+const prisma = new PrismaClient()
+
+server.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
 
 server.listen({
   port: SERVER_PORT
 }).then(() => {
-  console.log(`HTTP server running at port ${SERVER_PORT} 🚀`)
+  console.log(`HTTP server running on http://localhost:${SERVER_PORT} 🚀`)
 })
